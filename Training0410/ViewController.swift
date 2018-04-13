d810904dba316153ad580190ce67e38d47d9f7af
cyko <cyko@yahoo-inc.com>
2018-04-13 12:43:50 +0800

    Initial Commit

---
+++ Training0410/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Training0410
+//
+//  Created by  Chih-Yu Ko on 2018/4/13.
+//  Copyright © 2018年 Oath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
